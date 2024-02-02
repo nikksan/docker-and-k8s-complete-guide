6c0463836d5aee62b2f2b000e6d37d5f01323934
Nikola Mihaylov <nikolamihaylov@Nikolas-MacBook-Pro.local>
2024-02-02 13:44:37 +0200

    Revert

--- tests/index.test.js
+++ tests/index.test.js
@@ -16,6 +16,6 @@ describe('HTTP server', () => {
     it('should return 200 on GET /', async () => {
         await request(app)
             .get('/')
-            .expect(201);
+            .expect(200);
     });
 });
